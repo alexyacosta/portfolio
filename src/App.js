6368640853531660a34fa0559f1fe43c4284a14b
Alexya Costa <alexya@Alexyas-MacBook-Air.local>
2022-10-26 21:11:09 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import Header from "./components/Header";
+
+
+function App() {
+
+  return (
+    <div id="portfolio">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+import '../style/Header.css';
+
+class Header extends Component {
+  titles = [];
+
+  constructor() {
+    super();
+    this.state = { checked: false };
+  }
+
+  render() {
+    return (
+      <header>
+
+        <div className="title-container">
+          <h1>Hello! <br/>
+            my name is
+          </h1>
+          <p>A<a class="ex1">lexy</a>a</p>
+          <h1>Welcome to my website</h1>
+
+        </div>
+
+
+      </header>
+    );
+  }
+}
+
+export default Header;
