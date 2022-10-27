057e77a742166e0af7b7804c4944681683e73fc5
Alexya Costa <alexya@Alexyas-MacBook-Air.local>
2022-10-27 12:31:59 -0300

    adicao tela about

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
 import Header from "./components/Header";
+import About from "./components/About";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div id="portfolio">
       <Header />
+      <About />
     </div>
   )
 }
