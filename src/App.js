69a57d2449763f64e33a63b0662e99fc49c16431
alexyacosta <Alexyacosta@live.com>
2023-02-28 02:02:12 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Button } from "bootstrap";
 import { React, createContext, useState } from "react";
 import ReactSwitch from "react-switch";
 
@@ -7,18 +8,23 @@ import Header from "./Layouts/Header";
 export const ThemeContext = createContext(null);
 
 function App() {
+  const darkImg = require('./Assets/Images/dark-luz.png');
+  const lightImg = require('./Assets/Images/light-luz.png');
+
   const [theme, setTheme] = useState("light");
 
   const toggleTheme = () => {
     setTheme((curr) => (curr === "light" ? "dark" : "light"));
   };
+    
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       <div className="App" id={theme}>
         
-        <div className="switch">
-          {/* <label>{theme === "light" ? "Light Mode" : "DarkMode"} </label> */}
-          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+        <div className="button">
+           {/* <label>{theme === "light" ? <img src={lightImg}/> : <img src={darkImg}/> } </label>  */}
+          <button className="toggle" onClick={toggleTheme} checked={theme === "dark"}>
+            </button>
         </div>
         <Header />
       </div>
